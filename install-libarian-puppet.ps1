e4711560736481d04c7b81f5f3d5675ff80d2f43
st01tkh <st01tkh@gmail.com>
2016-04-07 03:19:42 +0300

    Renamed

--- install-libarian-puppet.ps1
+++ install-libarian-puppet.ps1
@@ -7,4 +7,5 @@ cd librarian_puppet
 puppet module build
 puppet module install pkg\*.tar.gz --ignore-dependencies
 cd ..
+remove-item -recurse -force librarian_puppet
 puppet apply %SYSTEMDRIVE%\ProgramData\PuppetLabs\puppet\etc\modules\librarian_puppet\tests\init.pp
