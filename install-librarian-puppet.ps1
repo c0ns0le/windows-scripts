9d1ac56009f2ded6c1f25c107c95b73b611d718b
st01tkh <st01tkh@gmail.com>
2016-04-07 04:00:42 +0300

    Restored

---
+++ install-librarian-puppet.ps1
@@ -0,0 +1,17 @@
+#iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+#choco feature enable --name allowGlobalConfirmation # stop the -y flag being needed for all "choco install"s
+
+#choco install -f git 
+#choco install -f puppet
+
+remove-item -recurse -force librarian_puppet
+puppet module install puppetlabs-stdlib
+puppet module install chocolatey-chocolatey
+puppet module install basti1302-windows_path
+git clone https://github.com/st01tkh/puppet-librarian_puppet librarian_puppet
+cd librarian_puppet
+puppet module build
+puppet module install pkg\*.tar.gz --ignore-dependencies
+cd ..
+remove-item -recurse -force librarian_puppet
+puppet apply %SYSTEMDRIVE%\ProgramData\PuppetLabs\puppet\etc\modules\librarian_puppet\tests\init.pp
